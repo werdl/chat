8d9efddd8c0dd33f0d70e423e0b068ad3ddc0aa2
= <=>
2023-03-10 14:17:41 +0000

    chanbeg

--- words.py
+++ words.py
@@ -17,13 +17,3 @@ class WordMaths:
 import re,string
 
 input = " why 1 +2"
-
-# if re.match(r"[\w\s]*[\d\+\-\*\/]+[\w\s]*", input): # regex from https://stackoverflow.com/questions/38649496/python-determine-if-a-string-contains-math
-#     # do whatever you want
-#     alpa=list(string.printable)
-#     n=string.digits+"/"+"-"+"+"+"*"
-#     for y in n:
-#         alpa=str(alpa).replace(str(y),"")
-#     for x in alpa:
-#         input=input.replace(x,"")
-#     print(eval(input))
