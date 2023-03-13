262e141762449f4da86663b1931f6b94e86b1e5b
werdl <116349156+werdl@users.noreply.github.com>
2023-03-13 17:47:25 +0000

    Update words.py

--- words.py
+++ words.py
@@ -1,4 +1,6 @@
 import string
+import imdb
+im=imdb.IMDb()
 class WordMaths:
     def __init__(self,c):
         self.c=c
@@ -17,3 +19,30 @@ class WordMaths:
         except: return False
     def maths(self):
         return [self.expr,self.ans]
+class Movie:
+    def __init__(self,moviename):
+        self.moviename=moviename
+    def search(self):
+        result=im.search_movie(self.moviename)
+        self.topresult=result[0]['title']
+        self.topid=result[0].movieID
+    def get(self):
+        return {'title':self.topresult,'id':self.topid}
+    def rate(self):
+        code=self.topid
+        series=im.get_movie(code)
+        self.toprate=series.data['rating']
+        return self.toprate
+    def good(self):
+        if self.toprate>7:
+            return 2
+        elif self.toprate>4:
+            return 1
+        else:
+            return 0
+    def alli(self): # all in one for ease of use
+        self.search()
+        self.rate()
+        return self.good()
+# y=Movie("Hunger Games")
+# print(y.alli())
